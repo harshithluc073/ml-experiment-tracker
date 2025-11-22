e5a0c98d073165c4ade76f49f0f44c7ffbfeffd3
harshithluc073 <chitikeshiharshith@gmail.com>
2025-11-22 11:47:34 +0530

    Fix circular import in experiment tracker

--- src/experiment_tracker/__init__.py
+++ src/experiment_tracker/__init__.py
@@ -7,10 +7,10 @@ Zero-friction ML experiment tracking with intelligent fallbacks.
 Basic usage:
 
     >>> from experiment_tracker import ExperimentTracker
-    >>> tracker = ExperimentTracker(project_name="my_project")
-    >>> tracker.log_params({"learning_rate": 0.001})
-    >>> tracker.log_metrics({"loss": 0.5}, step=1)
-    >>> tracker.finish()
+    >>> 
+    >>> with ExperimentTracker(project_name="my_project") as tracker:
+    ...     tracker.log_params({"learning_rate": 0.001})
+    ...     tracker.log_metrics({"loss": 0.5}, step=1)
 
 :copyright: (c) 2024 by Harshith.
 :license: MIT, see LICENSE for more details.
@@ -20,14 +20,22 @@ __version__ = "0.1.0"
 __author__ = "Harshith"
 __email__ = "chitikeshiharshith@gmail.com"
 
-# Import main classes (will be implemented in later steps)
-# from experiment_tracker.core.tracker import ExperimentTracker
-# from experiment_tracker.core.config import Config
-# from experiment_tracker.core.run import Run
+# Import main classes - ORDER MATTERS!
+# Import dependencies first
+from experiment_tracker.core.config import Config
+from experiment_tracker.core.run import Run, RunManager
+from experiment_tracker.utils.env_detector import detect_environment, EnvironmentDetector
+
+# Then import tracker (which depends on the above)
+from experiment_tracker.core.tracker import ExperimentTracker, track_experiment
 
 __all__ = [
     "__version__",
-    # "ExperimentTracker",
-    # "Config",
-    # "Run",
+    "ExperimentTracker",
+    "track_experiment",
+    "Config",
+    "Run",
+    "RunManager",
+    "detect_environment",
+    "EnvironmentDetector",
 ]
--- src/experiment_tracker/core/__init__.py
+++ src/experiment_tracker/core/__init__.py
@@ -1,3 +1,13 @@
 """Core experiment tracking functionality."""
 
-__all__ = []
+from experiment_tracker.core.config import Config
+from experiment_tracker.core.run import Run, RunManager
+from experiment_tracker.core.tracker import ExperimentTracker, track_experiment
+
+__all__ = [
+    'Config',
+    'Run', 
+    'RunManager',
+    'ExperimentTracker',
+    'track_experiment'
+]
