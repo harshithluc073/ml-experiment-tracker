dcf3e0705739a84e2f806986cc8a7a9d067880b2
harshithluc073 <chitikeshiharshith@gmail.com>
2025-11-22 12:41:23 +0530

    Update utils __init__.py with new imports

--- src/experiment_tracker/utils/__init__.py
+++ src/experiment_tracker/utils/__init__.py
@@ -1,15 +1,28 @@
-"""Utility functions and helpers."""
+"""Utility functions and classes."""
 
 from experiment_tracker.utils.env_detector import (
-    EnvironmentDetector,
     detect_environment,
-    show_setup_instructions
+    EnvironmentDetector,
+    EnvironmentType
 )
+from experiment_tracker.utils.artifact_manager import (
+    ArtifactManager,
+    Artifact,
+    ArtifactType
+)
+from experiment_tracker.utils.diff_generator import (
+    DiffGenerator,
+    RunComparison,
+    Diff,
+    DiffType
+)
+from experiment_tracker.utils.html_reporter import HTMLReporter
+from experiment_tracker.utils.auto_plotter import AutoPlotter
 
 __all__ = [
-    'EnvironmentDetector',
     'detect_environment',
-    'show_setup_instructions',
+    'EnvironmentDetector',
+    'EnvironmentType',
     'ArtifactManager',
     'Artifact',
     'ArtifactType',
